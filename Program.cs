8d89e5e8ad78c139a662b3dc6a9a8301f64c6257
marellanorero <marellanonorero@gmail.com>
2022-10-19 18:46:01 -0300

    Ejercicio 1

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Introduce your name");
+string nombre = Console.ReadLine();
+
+Console.WriteLine("Hello, " + nombre);
+
+DateTime localDate = DateTime.Now;
+Console.WriteLine(localDate);
